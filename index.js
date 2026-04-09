3024a78207a02f22de3ac0f7f4332816582abc09
PatMustardsTool <oliveraherne99@gmail.com>
2026-04-09 14:41:29 +0100

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let my_name;
 
 document.getElementById('my_submit_button').onclick = function () {
-    my_name = document.getElementById('my_name').value;
+    my_name = document.getElementById('my_name_input').value;
     console.log(my_name);
-    document.getElementById('my_header').value = `Hello, ${my_name}`;
+    document.getElementById('my_header').textContent = `Hello, ${my_name}`;
 }

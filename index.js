f15b83f9676d2914c864d6f4ddf14deda77b4b70
PatMustardsTool <oliveraherne99@gmail.com>
2026-04-09 16:33:57 +0100

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ my_submit_button.onclick = function () {
     const my_name = my_text_input.value;
     const my_number = my_number_input.value;
     my_answer= Math.round((my_number*MY_PI+my_answer)*100)/100;
-    console.log(my_answer);
+    console.log(`Answer: ${my_answer}`);
     let my_html= `Text: ${my_name}<br>Number: ${my_answer}`;
     document.getElementById('my_result').innerHTML = my_html;
 }

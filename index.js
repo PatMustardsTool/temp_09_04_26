f1d7e82f6d6aa49ff96f48c315205c6ee3a9e3b2
PatMustardsTool <oliveraherne99@gmail.com>
2026-04-10 08:46:12 +0100

    update

--- index.js
+++ index.js
@@ -6,30 +6,35 @@ const my_reset_button = document.getElementById('my_reset_button');
 const my_result_div = document.getElementById('my_result_div');
 
 let my_answer = 0;
-let my_html;
+let my_conditional_text;
 
-my_submit_button.onclick = function () {
+my_submit_button.onclick = function ()
+{
     const my_name = my_text_input.value;
     const my_number = my_number_input.value;
-    my_answer= Math.round((my_number*MY_PI+my_answer)*100)/100;
+    my_answer = Math.round((my_number*MY_PI+my_answer)*100)/100;
 
     if (my_answer < 10)
-        {
-            my_html= `Text: ${my_name}<br>Number: ${my_answer} (Weak)`;
-        }
+    {
+        my_conditional_text = 'Weak'
+    }
     else if (my_answer < 100)
-        {
-            my_html= `Text: ${my_name}<br>Number: ${my_answer} (Strong)`;
-        }
+    {
+        my_conditional_text = 'Strong'
+    }
     else
     {
-        my_html= `Text: ${my_name}<br>Number: ${my_answer} (Grand)`;
+        my_conditional_text = 'Grand'
     }
+
+    const my_html = `Text: ${my_name}<br>Number: ${my_answer} (${my_conditional_text})`;
+
     my_result_div.innerHTML = my_html;
     console.log(`Answer: ${my_answer}`);
 }
 
-my_reset_button.onclick = function () {
+my_reset_button.onclick = function ()
+{
     my_text_input.value = '';
     my_number_input.value = '';
     my_answer = 0;

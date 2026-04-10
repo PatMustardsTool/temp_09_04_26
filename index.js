2bbc6855e2229561a0c4d22188f061c14cf69c27
PatMustardsTool <oliveraherne99@gmail.com>
2026-04-10 12:58:51 +0100

    update

--- index.js
+++ index.js
@@ -1,42 +1,58 @@
 const MY_PI = Math.PI;
 const my_text_input = document.getElementById('my_text_input');
 const my_number_input = document.getElementById('my_number_input');
+const my_checkbox_input = document.getElementById('my_checkbox_input');
+const my_radio_group = document.getElementsByName('radio_grouping');
+
+// const my_radio_group = document.querySelectorAll('input[name="radio_grouping"]');
+
+
 const my_submit_button = document.getElementById('my_submit_button');
 const my_reset_button = document.getElementById('my_reset_button');
-const my_result_div = document.getElementById('my_result_div');
+const my_result_table = document.getElementById('my_result_table');
 
 let my_answer = 0;
-let my_conditional_text;
 
 my_submit_button.onclick = function ()
 {
     const my_name = my_text_input.value;
     const my_number = my_number_input.value;
+    const my_checkbox_status = my_checkbox_input.checked;
+    const my_radio_value = Array.from(my_radio_group).find(my_radio => my_radio.checked)?.value;
+    console.log(my_radio_value)
     my_answer = Math.round((my_number*MY_PI+my_answer)*100)/100;
 
+    let my_conditional_text;
     if (my_answer < 10)
     {
-        my_conditional_text = 'Weak'
+        my_conditional_text = 'Weak';
     }
     else if (my_answer < 100)
     {
-        my_conditional_text = 'Strong'
+        my_conditional_text = 'Strong';
     }
-    else
-    {
-        my_conditional_text = 'Grand'
+    else {
+        my_conditional_text = 'Grand';
     }
 
-    const my_html = `Text: ${my_name}<br>Number: ${my_answer} (${my_conditional_text})`;
+    const my_html = `
+        <table>
+          <tr><td>Text</td><td>${my_name}</td></tr>
+          <tr><td>Number</td><td>${my_answer} (${my_conditional_text})</td></tr>
+          <tr><td>Checkbox</td><td>${my_checkbox_status}</td></tr>
+          <tr><td>Radio</td><td>${my_radio_value}</td></tr>
+        </table>
+        `;
 
-    my_result_div.innerHTML = my_html;
-    console.log(`Answer: ${my_answer}`);
+    my_result_table.innerHTML = my_html;
 }
 
 my_reset_button.onclick = function ()
 {
     my_text_input.value = '';
     my_number_input.value = '';
+    my_checkbox_input.checked = false;
+    Array.from(my_radio_group).forEach(my_radio => my_radio.checked = false);
     my_answer = 0;
-    my_result_div.innerHTML = '';
+    my_result_table.innerHTML = '';
 }
